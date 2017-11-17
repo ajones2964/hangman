65a7d03f49c0befa89ead63bed1f0d1991de06d9
InfamousHacker4chan <ajones2964@greenvilleschools.us>
2017-11-17 09:30:45 -0500

    closer

--- hangman.py
+++ hangman.py
@@ -1,5 +1,7 @@
+tries = 0
 def get_puzzle():
-    pass
+    puzzle = "minceraft"
+    return puzzle.lower()
 
 def get_solved(puzzle, guesses):
     solved = ""
@@ -13,15 +15,60 @@ def get_solved(puzzle, guesses):
     return solved
 
 def get_guess():
-    pass
+    letter = input("Guess a letter: ")
+    return letter.lower()
 
 def display_board(solved):
-    pass
+    print(solved)
+
+def graphic(tries):
+    if tries == 0:
+        print("""
+GCSD                Games
+_________________________
+Guess the word right to save
+your Games from evil GCSD""")
+    if tries == 1:
+        print("""   GCSD             Games
+_________________________""")
+    elif tries == 2:
+        print("""     GCSD           Games
+_________________________""")
+    elif tries == 3:
+        print("""         GCSD       Games
+_________________________""")
+    elif tries == 4:
+        print("""             GCSD   Games
+_________________________""")
+    elif tries == 5:
+        print("""                GCSDGames
+_________________________""")
+    elif tries == 6:
+        print("""              URL Blocked
+_________________________""")
 
 def show_result():
-    pass
+    print("You Win")
     
 def play():
-    pass
+    puzzle = get_puzzle()
+    guesses = ""
+    tries = 0
+    solved = get_solved(puzzle, guesses)
+
+    limit = 6
+
+    while solved != puzzle or tries != limit:
+        display_board(solved)
+        graphic(tries)
+        letter = get_guess()
+
+        if letter not in puzzle:
+            tries += 1
+        guesses += letter
+        solved = get_solved(puzzle, guesses)
+        
+
+    show_result()
     
 play()
