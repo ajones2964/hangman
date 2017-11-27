1e517bb6469558542639465f8defaad29d88ba68
InfamousHacker4chan <ajones2964@greenvilleschools.us>
2017-11-26 20:56:37 -0500

    Finished

--- hangman.py
+++ hangman.py
@@ -1,74 +1,127 @@
-tries = 0
+#Hangman, the game
+#Jones, Ashton
+import random
+
+def start_screen():
+    print("""~~~~~~~GCSD~~~~~~~~
+Destroyer of Dreams
+""")
+
+def end_screen():
+    print("""           GCSD
+       URL Blocked
+URL:www.coolmath-game.com
+
+     category: games
+
+coolmath is blocked regardless if you win or not.
+
+Ashton 11/14/17""")
+
 def get_puzzle():
-    puzzle = "minceraft"
-    return puzzle.lower()
+    rand_puzzle = ["Minceraft", "Roblox", "CoopDogg", "CoolMath", "Starbound", "Cats", "Python", "Kindle", "Windows", "MSpaint", "Pepe", "Memes"]
+    puzzlechoice = random.randint(0, 11)
+    return rand_puzzle[puzzlechoice]
 
 def get_solved(puzzle, guesses):
     solved = ""
-
     for letter in puzzle:
-        if letter in guesses:
+        if letter.lower() in guesses.lower():
             solved += letter
         else:
             solved += "-"
+    return (solved)
 
-    return solved
 
+def check_strikes(puzzle, guesses, strikes):
+        if guesses[-1] in puzzle:
+            return strikes
+        strikes += 1
+        return strikes
+            
 def get_guess():
-    letter = input("Guess a letter: ")
-    return letter.lower()
-
-def display_board(solved):
+    print()
+    letter = input("Enter a Letter to guess the word.")
+    print()
+    if len(letter) <= 1:
+        return str(letter)
+    else:
+        print("You can only type in one letter at a time you fool!")
+        return get_guess()
+            
+def display_board(solved, guesses, strikes):
     print(solved)
+    if strikes == 1:
+        print("GCSD             coolmath")
+    elif strikes == 2:
+        print("  GCSD           coolmath")
+    elif strikes == 3:
+        print("    GCSD         coolmath")
+    elif strikes == 4:
+        print("      GCSD       coolmath")
+    elif strikes == 5:
+        print("        GCSD     coolmath")
+    elif strikes == 6:
+        print("          GCSD   coolmath")
 
-def graphic(tries):
-    if tries == 0:
-        print("""
-GCSD                Games
-_________________________
-Guess the word right to save
-your Games from evil GCSD""")
-    if tries == 1:
-        print("""   GCSD             Games
-_________________________""")
-    elif tries == 2:
-        print("""     GCSD           Games
-_________________________""")
-    elif tries == 3:
-        print("""         GCSD       Games
-_________________________""")
-    elif tries == 4:
-        print("""             GCSD   Games
-_________________________""")
-    elif tries == 5:
-        print("""                GCSDGames
-_________________________""")
-    elif tries == 6:
-        print("""              URL Blocked
-_________________________""")
 
-def show_result():
-    print("You Win")
-    
-def play():
-    puzzle = get_puzzle()
-    guesses = ""
-    tries = 0
-    solved = get_solved(puzzle, guesses)
+def show_result(strikes, limit, puzzle):
+    if strikes == limit:
+        print("""           GCSD
+       URL Blocked
+URL:www.coolmath-game.com
 
-    limit = 6
+     category: games
 
-    while solved != puzzle or tries != limit:
-        display_board(solved)
-        graphic(tries)
-        letter = get_guess()
 
-        if letter not in puzzle:
-            tries += 1
-        guesses += letter
-        solved = get_solved(puzzle, guesses)
+You lost, R.I.P coolmath. The word was """ + puzzle)
+    else:
+        print()
+        print("You got it in " + str(limit - strikes) + " guesses.")
         
 
-    show_result()
-    
-play()
+def play():
+    strikes = 0
+    limit = 7
+    puzzle = get_puzzle()
+    guesses = ""
+    solved = get_solved(puzzle, guesses)
+    display_board(solved, guesses, strikes)
+
+    while solved != puzzle and strikes != limit:
+        if strikes != limit:
+            print("You have " + str(limit - strikes) + " guesses left until GCSD blocks www.coolmath-games.com")
+            print()
+            guesses += get_guess()
+            solved = get_solved(puzzle, guesses)
+            strikes = check_strikes(puzzle, guesses, strikes)
+            display_board(solved, guesses, strikes)
+        else:
+            print("You ran out of guesses fam wow.")
+            print()
+
+    show_result(strikes, limit, puzzle)
+
+def play_again():
+    print()
+    while True:
+        decision = input("Would you like to play again? (y/n) ")
+        decision = decision.upper()
+        print()
+        if decision == 'Y' or decision == 'YES':
+            return True
+        elif decision == 'N' or decision == 'NO':
+            return False
+        else:
+            print()
+            print("Answer it right you mongrel. Would you like to play again? (y/n)")
+
+
+start_screen()
+
+playing = True
+while playing == True:
+    play()
+    playing = play_again()
+
+end_screen()
